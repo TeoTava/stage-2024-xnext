8f35bd45621ea96b137996831e1d2173ee476e4f
Matteo <tavazzi.matteo@gmail.com>
2024-06-19 13:07:36 +0200

    Particolare aggiunta del file memoria_macchine.txt, lavoro del progetto sull'implementazione di un sistema di memoria, recupero e salvataggio dei dati

--- gestione_macchine.c
+++ gestione_macchine.c
@@ -8,14 +8,6 @@ struct Macchina {
     float tensione, corrente;
 };
 
-// TODO una funzione che riceve in ingresso una macchina e ne stampa TUTTI i campi
-// sarà usata dalla ricerca della macchina con potenza maggiore
-// nonchè dalla scelta 1)
-
-// DOPODICHE commit
-
-// DOPODOPODICHE lavoriamo con i files
-
 void clear_buffer () {
     int c;
     while ((c=getchar())!='\n' && c!= EOF);
@@ -25,13 +17,15 @@ int acq_int (int mess) {
     int x, risultato;
     
     switch (mess) {
-        case 0: printf("Scegliere un'opzione fornendo il numero:\n1. Visualizzare i dati di una macchina fornendo il codice\n2. Aggiungere una nuova macchina\n3. Visualizzare che macchina ha potenza maggiore\n4. Fine Programma\n");
+        case 0: printf("Scegliere un'opzione fornendo il numero:\n0. Visualizza tutte le macchine con tutte le informazioni\n1. Salvare le nuove macchine in memoria\n2. Visualizzare i dati di una macchina fornendo il codice\n3. Aggiungere una nuova macchina\n4. Visualizzare che macchina ha potenza maggiore\n5. Fine Programma\n");
         break;
         case 1: printf("Fornisci il numero effettivo delle macchine [numero intero positivo]\n");
         break;
         case 2: printf("Fornisci codice della macchina [numero intero]\n");
         break;
         case 3: printf("Fornisci codice della macchina nuova [numero intero]\n");
+        break;
+        case 4: printf("Scegliere tra le seguenti opzioni:\n1. Riprendere le macchine precedentemente salvate nella memoria\n2. Inserire macchine da zero\n");
         break;
         default: printf("Fornisci un valore intero\n");
     }
@@ -61,9 +55,9 @@ int acq_int (int mess) {
 
 void acq_string(struct Macchina *M, int i, int mess) {
     switch (mess) {
-        case 0: printf("Fornisci lo scopo della macchina [stringa]\n");
+        case 0: printf("Fornisci lo scopo della macchina [stringa, 1 sola parola]\n");
         break; 
-        case 1: printf("Fornisci lo scopo della macchina nuova [stringa]\n");
+        case 1: printf("Fornisci lo scopo della macchina nuova [stringa, 1 sola parola]\n");
         break; 
         default: printf("Fornisci una stringa\n");
     }
@@ -117,6 +111,10 @@ char acq_char (int mess) {
     switch (mess) {
         case 1: printf("Vuoi visualizzare tutti i codici possibili? [Y/N]\n");
         break;
+        case 2: printf("Vuoi salvare le macchine non ancora registrate in memoria? [Y/N]\n");
+        break;
+        case 3: printf("Vuoi salvare quest'ultima macchina aggiunta in memoria? [Y/N]\n");
+        break;
         default: printf("Fornisci un carattere letterale\n");
     }
 
@@ -131,7 +129,7 @@ char acq_char (int mess) {
                 clear_buffer();
             }
             else {
-                if (mess==1) {
+                if (mess==1 || mess==2 || mess==3) {
                     if (x!='y' && x!='Y' && x!='n' && x!='N') {
                         printf("Inserire valore valido: non è stata inserita una scelta possibile\n");
                     }
@@ -154,22 +152,59 @@ char acq_char (int mess) {
 
 
 int main () {
-    int scelta, N, i=0, dim=10000;
+    int scelta, N, i=0, dim=10000, controllo=0;
     struct Macchina M[dim];
+    FILE *file_point;
+    char file_memoria[21]="memoria_macchine.txt";
 
-    do {
-        N=acq_int(1);
-    } while (N<=0 || N>dim);
+    while (controllo==0) {
+        scelta=acq_int(4);
+        
+        switch (scelta) {
+            case 1: {
+                controllo=1;
 
-    for (i=0; i<N; i++) {
-        printf("MACCHINA NUMERO %d\n", i+1);
+                file_point=fopen(file_memoria, "r");
+                if (file_point==NULL) {
+                    printf("Errore di apertura del file\n");
+                    break;
+                }
 
-        do {
-            M[i].codice=acq_int(2);
-            acq_string(M, i, 0);
-            M[i].tensione=acq_float(0);
-            M[i].corrente=acq_float(1);
-        } while (M[i].tensione<=0 || M[i].corrente<=0);
+                fscanf(file_point, "%d", &N);
+
+                for (i=0; i<N; i++) {
+                    fscanf(file_point, "%d %s %f %f", &M[i].codice, M[i].scopo, &M[i].tensione, &M[i].corrente);
+                }
+
+                fclose(file_point);
+                printf("Valori ripristinati correttamente\n");
+            }
+            break; 
+
+            case 2: {
+                controllo=1;
+
+                do {
+                    N=acq_int(1);
+                } while (N<=0 || N>dim);
+
+                for (i=0; i<N; i++) {
+                    printf("MACCHINA NUMERO %d\n", i+1);
+
+                    do {
+                        M[i].codice=acq_int(2);
+                        acq_string(M, i, 0);
+                        M[i].tensione=acq_float(0);
+                        M[i].corrente=acq_float(1);
+                    } while (M[i].tensione<=0 || M[i].corrente<=0);
+                }
+            }
+            break;
+            
+            default: {
+                printf("Inserire una scelta valida\n");
+            }
+        }
     }
 
     do {
@@ -177,6 +212,23 @@ int main () {
 
         switch (scelta) {
             case 1: {
+                file_point = fopen (file_memoria, "w");
+                if (file_point==NULL) {
+                    printf("Errore nell'apertura del file\n");
+                    break;
+                }
+                
+                fprintf(file_point, "%d\n", N);
+                for (i=0; i<N; i++) {
+                    fprintf(file_point, "%d %s %f %f\n", M[i].codice, M[i].scopo, M[i].tensione, M[i].corrente);
+                }
+
+                fclose(file_point);
+                printf("Informazioni delle macchine salvate correttamente su \"%s\"\n", file_memoria);
+            }
+            break;
+
+            case 2: {
                 int nuovo, flag=0; 
 
                 while (1) {
@@ -209,7 +261,9 @@ int main () {
             }
             break;
 
-            case 2: {
+            case 3: {
+                char salva;
+                
                 if (N==dim) {
                     printf("Aggiunta impossibile: vettore dati già pieno.\n");
                 }
@@ -223,10 +277,27 @@ int main () {
 
                     N++;
                 }
+
+                salva=acq_char(3);
+                if (salva=='Y') {
+                    file_point = fopen (file_memoria, "w");
+                    if (file_point==NULL) {
+                        printf("Errore nell'apertura del file\n");
+                        break;
+                    }
+                
+                    fprintf(file_point, "%d\n", N);
+                    for (i=0; i<N; i++) {
+                        fprintf(file_point, "%d %s %f %f\n", M[i].codice, M[i].scopo, M[i].tensione, M[i].corrente);
+                    }
+
+                    fclose(file_point);
+                    printf("Informazioni delle macchine salvate correttamente su \"%s\"\n", file_memoria);
+                }
             }
             break;
 
-            case 3: {
+            case 4: {
                 float max, potenza;
 
                 for (i=0; i<N; i++) {
@@ -250,8 +321,16 @@ int main () {
                 }
             }
             break;
+
+            case 0: {
+                printf("Ci sono un totale di %d macchine\n", N);
+                
+                for (i=0; i<N; i++) {
+                    printf("MACCHINA %d\n - Codice: %d\n - Scopo: %s\n - Tensione = %f V\n - Corrente = %f A\n", i+1, M[i].codice, M[i].scopo, M[i].tensione, M[i].corrente);
+                }
+            }
         }
-    } while (scelta!=4);
+    } while (scelta!=6);
 
     return 0;
 }
